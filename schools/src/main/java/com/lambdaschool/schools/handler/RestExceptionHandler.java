8e1de3cf8f0417b5fa87f8c7dc75697843073be0
Chris King <cking5836@gmail.com>
2020-08-17 17:30:59 -0400

    MVP

--- schools/src/main/java/com/lambdaschool/schools/handler/RestExceptionHandler.java
+++ schools/src/main/java/com/lambdaschool/schools/handler/RestExceptionHandler.java
@@ -12,6 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
@@ -41,16 +42,31 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
     }
 
     @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(NoHandlerFoundException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        ErrorDetail errorDetail = new ErrorDetail();
+        errorDetail.setTimestamp(new Date());
+        errorDetail.setStatus(status.value());
+        errorDetail.setTitle("Rest Internal Exception");
+        errorDetail.setDetail("Found an issue with School: " + ex.getMessage());
+        errorDetail.setDeveloperMessage(ex.getClass().getName());
+        errorDetail.setErrors(helperFunctions.getConstraintViolation(ex));
+
+        return new ResponseEntity<>(errorDetail, null, status);
+    }
+
+    @Override
     protected ResponseEntity<Object> handleExceptionInternal(Exception ex, Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
 
         ErrorDetail errorDetail = new ErrorDetail();
         errorDetail.setTimestamp(new Date());
         errorDetail.setStatus(status.value());
         errorDetail.setTitle("Rest Internal Exception");
-        errorDetail.setDetail(ex.getMessage());
+        errorDetail.setDetail("Found an issue with School: " + ex.getMessage());
         errorDetail.setDeveloperMessage(ex.getClass().getName());
         errorDetail.setErrors(helperFunctions.getConstraintViolation(ex));
 
         return new ResponseEntity<>(errorDetail, null, status);
     }
+
+
 }
--- schools/src/main/java/com/lambdaschool/schools/services/StudentServiceImpl.java
+++ schools/src/main/java/com/lambdaschool/schools/services/StudentServiceImpl.java
@@ -17,8 +17,7 @@ import java.util.List;
  * Implements the StudentService Interface
  */
 @Service(value = "studentService")
-public class StudentServiceImpl
-    implements StudentService
+public class StudentServiceImpl implements StudentService
 {
     /**
      * Connects this service to the Student table.
@@ -58,7 +57,7 @@ public class StudentServiceImpl
     public void delete(long id)
     {
         studentrepos.findById(id)
-            .orElseThrow(() -> new EntityNotFoundException("Student id " + id + " not found!"));
+            .orElseThrow(() -> new ResourceNotFoundException("Student id " + id + " not found!"));
         studentrepos.deleteById(id);
     }
 
@@ -71,7 +70,7 @@ public class StudentServiceImpl
         if (student.getStudentid() != 0)
         {
             Student oldStudent = studentrepos.findById(student.getStudentid())
-                .orElseThrow(() -> new EntityNotFoundException("Student id " + student.getStudentid() + " not found!"));
+                .orElseThrow(() -> new ResourceNotFoundException("Student id " + student.getStudentid() + " not found!"));
 
             // delete the courses for the old student we are replacing
             for (StudCourses ur : oldStudent.getCourses())
